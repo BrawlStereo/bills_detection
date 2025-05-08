4c52b9829f5b52000562f0ff81a05706cafe96cc
Ana Paola Jiménez Sedano <143899647+AnaJmz345@users.noreply.github.com>
2025-05-07 23:08:36 -0600

    Implement text detection with ocr
    Extracted each bill using detected contours and applied OCR to recognize its value

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
+import easyocr
 
-# ---Charges the image to analyze ---
+# Charges the image to analyze
 image = cv2.imread("bills_total.jpg")
 if image is None:
     print("Image not found")
     exit()
+image_original = image.copy() #copy in which the OCR analisis will be made
 
 # Converts the image to grayscale so binarization accepts it
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -36,5 +38,48 @@ cv2.drawContours(
     contours,      #list of arrays with contours coordenates 
     -1,            #draw all contours
     (0, 0, 255),   #red contours
-    1
-)                  #line width of 1
+    1              #line width of 1
+)                  
+
+# Easyocr initialization to spanish because the bills are mexican
+reader = easyocr.Reader(['es'], gpu=False)
+
+number_bills_counter = {}  # dictionary to count bills of each type
+total = 0    # total sum of the money shown
+
+# Process every contour 
+for i, contour in enumerate(contours):
+
+    #Ignores contours that aren't bills
+    area = cv2.contourArea(contour)
+    if area < 15000:
+        continue
+
+    # Cuts the image of the bill from the unmodified image
+    x, y, w, h = cv2.boundingRect(contour)      #gets contour dimensions, x is initial position in x, y is the initial position in y,
+                                            #w the bill width and h the bill height
+    bill = image_original[y:y+h, x:x+w]  #gets the portion of the image since line y until y+h and since column x until x+w
+
+    # Applies gussian blur to eliminate small details that ocr could detect
+    bill_blurred = cv2.GaussianBlur(bill, (5, 5), 0)
+
+    # Apply OCR
+    results = reader.readtext(bill_blurred)
+    #print(f"OCR en bill {i+1} (con blur):")
+
+    detected_value = None #bill's value detected by ocr
+
+    # Look if the text detected by ocr is a valid bill value
+    for _, text, _ in results:
+        #text = text.replace("$", "").replace(",", "").strip()     #deletes possible unnecesary symbols detected
+        print("→ Detected:", text)
+        text = text.lower().replace("o", "0").replace("s", "5").replace("l", "1") #replaces posibble confussions between a char and int
+        for possible_value in [1000, 500, 200, 100, 50, 20]:
+            if str(possible_value) in text:
+                detected_value = possible_value
+                break
+        if detected_value:
+            break
+   
+    
+
