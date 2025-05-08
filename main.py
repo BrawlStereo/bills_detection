0ee08b8c3135687909853cffbe8e983cb46e42dd
Braulio Fernando Antero Díaz <152311701+BrawlStereo@users.noreply.github.com>
2025-05-07 22:21:34 -0600

    Add grayscake and binarization

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ image = cv2.imread("bills_total.jpg")
 if image is None:
     print("Image not found")
     exit()
+
+# Converts the image to  grayscale so binarization accepts it
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Applies adaptative binarization to the image so the original white background is black and the bills in white tones
+#which facilitates the bills contorus detection
+image_binary = cv2.adaptiveThreshold(
+    image_gray, 
+    255,                                #max value taken by pixels less than the threshold
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,     #calculate threshold with gaussian weighted sum
+    cv2.THRESH_BINARY_INV, 
+    11,                                 #size of pixel surrounding
+    2                                   #constant to add to gaussian result
+)
